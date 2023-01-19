9309c9e7d03b965eaf222ebc7e38baf92e3ca602
artem <ip.bakhmatov@mail.ru>
2023-01-19 18:38:44 +0300

    Правила и типы названия переменных

---
+++ script.js
@@ -0,0 +1,15 @@
+const vehicleBodyWidth = 5000,
+     vehicleBodyLength = 4000;
+
+console.log('Ширина кузова автомобиля: ' + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+// ['sdff', 'fsfwff', 'sdffffd'].map(a => )
+// Request
+/* data
+   response */
+
+// snake_case (название папок и файлов кроме переменных в JS)
+// UPPER_SNAKE_CASE (перем-е в верхнем регис-е в нижним _ , в JS появляются в виде констант)
+// например : const COLOR_RED = '#F00'
+// Kebab-case (название папок и файлов кроме переменных в JS)
+// PascalCase (название классов в JS)
+
