55f13a7e5d15e1db60a723aac8856f469c63c7d4
artem <ip.bakhmatov@mail.ru>
2023-01-27 23:05:09 +0300

    Цикл в цикле и метки

--- script.js
+++ script.js
@@ -1,106 +1,77 @@
-/* 
-const hamburger = true;			// гамбургер
-const fries = true;				// картошка фри
-
-if (hamburger && fries) {
-	console.log('Я сыт!');      // Я сыт!
-}
-
-console.log((hamburger && fries));  // true 
-*/
-
-/* 
-const hamburger = 5;			// гамбургер
-const fries = 0;				// картошка фри
-
-if (hamburger && fries) {
-	console.log('Я сыт!');      // тут условие невыполнится
-}
-
-console.log((hamburger && fries));  // 0 
-*/
-
-/* 
-const hamburger = 2;			// гамбургер
-const fries = 1;				// картошка фри
-
-if (hamburger === 3 && fries) {
-	console.log('Все сыты!');      // тут условие невыполнится
-} else {
-	console.log('Мы уходим!');	   // выполнится вот это условие 
-} 
-*/
-
-/* 
-const hamburger = 3;			// гамбургер
-const fries = 1;				// картошка фри
-const cola = 0;					// кола
-
-console.log(hamburger === 3 && cola && fries); // 0
-
-if (hamburger === 3 && cola === 1 && fries) {  // true && false && true
-	console.log('Все сыты!');      // тут условие невыполнится
-} else {
-	console.log('Мы уходим!');	   // выполнится вот это условие 
-} 
-
-// Когда оператор && наткнулся на первую ложь, условие останавливается
-// Когда оператор && наткнулся на правду, вывотся последняя правда (если несколько правдивых значений)
-
-console.log(1 && 0);		// 0
-console.log(1 && 5);		// 5
-console.log(null && 5);		// null
-console.log(0 && 'djhsh');	// 0 
-*/
-
-/* 
-const hamburger = 3;			// гамбургер
-const fries = 0;				// картошка фри
-const cola = 0;					// кола
-
-if (hamburger || cola || fries) {  // true && false && false
-	console.log('Все довольны!');      // выполнится условие
-} else {
-	console.log('Мы уходим!');	   
-}  
-*/
-
-// Когда ИЛИ ноходит первую правду условие прекщается
-// ИЛИ запинается на правде
+// for (let i = 0; i < 3; i++) {		0
+// 	console.log(i);						0
+// 	for (let j = 0; j < 3; j++) {		1
+// 		console.log(j);					2
+// 	}									1
+// }									0
+// 										1
+// 										2
+// 										2
+// 										0
+// 										1
+// 										2
 
 
-/* 
-const hamburger = 0;			// гамбургер
-const fries = 0;				// картошка фри
-const cola = 0;					// кола
 
-if (hamburger || cola || fries) {  // false && false && false
-	console.log('Все довольны!');      // Не выполнится условие
-} else {
-	console.log('Мы уходим!');	   	   // выполнится условие
-}  
-*/
+// let result = '';
+// const length = 7;
 
-// Если всё неправда то возвращает последнее ложное значение
+// for (let i = 0; i < length; i++) {
+// 	for (let j = 0; j < i; j++) {
+// 		result += '*';
+// 	}
+// 	result += '\n';
+// }
 
-let JohnReport, alexReport, samReport, mariaReport = 'done';
+// console.log(result);			   // Ответ // *
+// 											// **
+// 											// ***
+// 											// ****
+// 											// *****
+// 											// ******
 
-console.log(JohnReport || alexReport || samReport || mariaReport);  // done
+// first: for (let i = 0; i < 3; i++) { // first: -> Эта часть называется меткой
+// 	console.log(`First level: ${i}`);						
+// 	for (let j = 0; j < 3; j++) {		
+// 		console.log(`Second level: ${j}`);		
+// 		for (let k = 0; k < 3; k++) {	
+// 			if (k === 2) {
+// 				continue first;
+// 			}
+// 			console.log(`Third level: ${k}`);						
+// 		}					
+// 	}									
+// }	
 
-const hamburger = 3;			// гамбургер
-const fries = 3;				// картошка фри
-const cola = 0;					// кола
-const nuggets = 2;				// наггенсы
+// Ответ
+// First level: 0
+// Second level: 0
+// Third level: 0
+// Third level: 1
+// First level: 1
+// Second level: 0
+// Third level: 0
+// Third level: 1
+// First level: 2
+// Second level: 0
+// Third level: 0
+// Third level: 1
 
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {  // true && false || true && true
-	console.log('Все довольны!');      // выполнится условие
-} else {
-	console.log('Мы уходим!');	   	   
-}  
+first: for (let i = 0; i < 3; i++) { // first: -> Эта часть называется меткой
+	console.log(`First level: ${i}`);						
+	for (let j = 0; j < 3; j++) {		
+		console.log(`Second level: ${j}`);		
+		for (let k = 0; k < 3; k++) {	
+			if (k === 2) {
+				break first;
+			}
+			console.log(`Third level: ${k}`);						
+		}					
+	}									
+}	
 
-console.log(hamburger === 3 && cola === 2 || fries === 3 && nuggets);  		// 2
-console.log(hamburger === 3 && (cola === 2 || fries === 3) && nuggets);  	// 2
-
-console.log(!0); // true
- 
-
+// Ответ
+// First level: 0
+// Second level: 0
+// Third level: 0
+// Third level: 1
