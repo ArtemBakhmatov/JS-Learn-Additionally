49d0ba0083436ad0f52e32bb9b14d6fdb9289211
artem <ip.bakhmatov@mail.ru>
2023-01-30 14:51:40 +0300

    Про важность return

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 const usdCurr = 28;
-const eurCurr = 32;
+const discount = 0.9;
 
 function convert(amount, curr) {
-	console.log(curr * amount); // 14000
+	return curr * amount; 
+	// console.log(1); // Работать не будет, после return код останавливается
 }
 
-convert(500, usdCurr); // 14000
-convert(500, eurCurr); // 16000
+function promotion(result) {
+	console.log(result * discount);
+	//return undefined; он выполняется внутри JS, не видно нам
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+///////////////////////////////////////////////////////////////////////////////
+function test() {
+	for (let i = 0; i < 5; i++) {
+		console.log(i);
+		if (i === 3) {
+			return;
+		}
+	}
+	console.log('Done'); // не сработает так как return останавливает код
+}
+
+test();
+
+///////////////////////////////////////////////////////////////////////////////
+
+function doNothing() {};
+console.log(doNothing() === undefined);  // true
+
+///////////////////////////////////////////////////////////////////////////////
+// Консоль разработчика
+
+// > console.log('11'); // console -> это объект, .log() -> это метод
+//  11    // выводи в консоль значение, и т.к console.log() это функция, значит она далжна что то возвращать
+// < undefined  // возрвщает undefined, так будет с любой функцией в котором нет return
+
+// Важно: после return код переность нельзя вниз, внутри JS будет выглядеть так: return;
